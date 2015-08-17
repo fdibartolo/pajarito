5feec2675670eb5abefc0ee6b7c49a61199f85c2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-08-17 14:27:10 -0300

    poc images from server cache

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,18 @@
 class MainController < ApplicationController
   def index
   end
+
+  def photos
+    photos = [
+      { image_tag: view_context.image_tag('portfolio/01.jpg', class: "img-responsive"), categories: 'interior'},
+      { image_tag: view_context.image_tag('portfolio/02.jpg', class: "img-responsive"), categories: 'exterior gaviotas'},
+      { image_tag: view_context.image_tag('portfolio/03.jpg', class: "img-responsive"), categories: 'gaviotas'},
+      { image_tag: view_context.image_tag('portfolio/04.jpg', class: "img-responsive"), categories: 'interior'},
+      { image_tag: view_context.image_tag('portfolio/05.jpg', class: "img-responsive"), categories: 'interior'},
+      { image_tag: view_context.image_tag('portfolio/06.jpg', class: "img-responsive"), categories: 'gaviotas'},
+      { image_tag: view_context.image_tag('portfolio/07.jpg', class: "img-responsive"), categories: 'exterior'},
+      { image_tag: view_context.image_tag('portfolio/08.jpg', class: "img-responsive"), categories: 'interior'}
+    ]
+    render json: { photos: photos }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/templates/:path.html' => 'templates#template', :constraints => { :path => /.+/ }
 
+  get '/photos' => 'main#photos', defaults: { format: :json }
+
   root 'main#index'
 end
