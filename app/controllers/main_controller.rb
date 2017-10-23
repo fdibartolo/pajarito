71c8ec9e14c89958f0cc86895eb8d5714ecac4ea
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2017-10-23 20:16:56 -0300

    refactor pulling photos

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,16 +3,24 @@ class MainController < ApplicationController
   end
 
   def photos
-    photos = [
-      { image_tag: view_context.image_tag('portfolio/01.jpg', class: "img-responsive"), categories: 'interior'},
-      { image_tag: view_context.image_tag('portfolio/02.jpg', class: "img-responsive"), categories: 'exterior gaviotas'},
-      { image_tag: view_context.image_tag('portfolio/03.jpg', class: "img-responsive"), categories: 'gaviotas'},
-      { image_tag: view_context.image_tag('portfolio/04.jpg', class: "img-responsive"), categories: 'interior'},
-      { image_tag: view_context.image_tag('portfolio/05.jpg', class: "img-responsive"), categories: 'interior'},
-      { image_tag: view_context.image_tag('portfolio/06.jpg', class: "img-responsive"), categories: 'gaviotas'},
-      { image_tag: view_context.image_tag('portfolio/07.jpg', class: "img-responsive"), categories: 'exterior'},
-      { image_tag: view_context.image_tag('portfolio/08.jpg', class: "img-responsive"), categories: 'interior'}
-    ]
+    photos = []
+    Image.active.each do |image|
+      photos << {
+        image_tag: view_context.image_tag(image.path, class: "img-responsive"), 
+        categories: image.categories
+      }
+    end
+    
+    # photos = [
+    #   { image_tag: view_context.image_tag('portfolio/01.jpg', class: "img-responsive"), categories: 'interior'},
+    #   { image_tag: view_context.image_tag('portfolio/02.jpg', class: "img-responsive"), categories: 'exterior gaviotas'},
+    #   { image_tag: view_context.image_tag('portfolio/03.jpg', class: "img-responsive"), categories: 'gaviotas'},
+    #   { image_tag: view_context.image_tag('portfolio/04.jpg', class: "img-responsive"), categories: 'interior'},
+    #   { image_tag: view_context.image_tag('portfolio/05.jpg', class: "img-responsive"), categories: 'interior'},
+    #   { image_tag: view_context.image_tag('portfolio/06.jpg', class: "img-responsive"), categories: 'gaviotas'},
+    #   { image_tag: view_context.image_tag('portfolio/07.jpg', class: "img-responsive"), categories: 'exterior'},
+    #   { image_tag: view_context.image_tag('portfolio/08.jpg', class: "img-responsive"), categories: 'interior'}
+    # ]
     render json: { photos: photos }
   end
 
