08e5504a4ceb6ad09dea4709d35f0fff8de25364
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2017-11-07 11:59:56 -0300

    add another email recipient

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "contact@gaviotas.com"
-  default to: "fernando.di.bartolo@gmail.com"
+  default from: "contacto@gaviotas.com"
+  default to: "fernando.di.bartolo@gmail.com, contacto.vuelagaviota@gmail.com"
   layout 'mailer'
 end
